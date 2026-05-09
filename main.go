fc0d5a110cb733772c5653f7a9b46df1c8f03e6a
Duc Huy LY <lyduchuy@proton.me>
2026-05-09 18:22:52 +0200

    write possible commands

--- main.go
+++ main.go
@@ -2,14 +2,38 @@ package main
 
 import (
 	"fmt"
+	"flag"
 	"os"
 )
 
 func main(){
+	help := flag.Bool("h", false, "show help")
+	flag.Parse()
+
+	if *help{
+		printHelp()
+		return
+	}
+
 	if len(os.Args) < 2 {
 		fmt.Println("Hello world")
 		return
 	}
-	arg := os.Args[1]
-	fmt.Println(arg)
+
+	command := os.Args[1]
+	possibleCommands := []string{"add", "delete", "update", "mark-in-progress", "mark-done", "list"}
+	
+	for _, m := range possibleCommands {
+		if m == command {
+			fmt.Println(m)
+			return
+		}
+	}
+
+	fmt.Println("command not not exist")
+
+}
+
+func printHelp() {
+	fmt.Println("Help for the cli-app")
 }
