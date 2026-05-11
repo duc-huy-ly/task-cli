01213673f3d6a7d82faf20e0c0faca2960afdd91
Duc Huy LY <lyduchuy@proton.me>
2026-05-12 01:09:36 +0200

    using a switch for main

--- main.go
+++ main.go
@@ -2,22 +2,22 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strings"
-
 	"github.com/dly/task-cli/core"
+	"os"
+	"strconv"
+	"strings"
 )
 
 const dataDirectory = "data/tasks.json"
 
 func main() {
 	checkFileExistence()
-	data := loadDataFromFile() 
+	data := loadDataFromFile()
 	user := core.App{}
 	user.LoadData(data)
 	commandStr, args := parseUserInput(os.Args)
-	if commandStr== "" {
-		fmt.Println("No command given")
+	if commandStr == "" {
+		fmt.Println("Welcome to task-cli. -h for help")
 		return
 	}
 	handleCommandStr(commandStr, args, &user)
@@ -25,30 +25,42 @@ func main() {
 }
 
 func handleCommandStr(commandStr string, args []string, user *core.App) {
-	action := strings.ToLower(commandStr)
-	if action == "add" && len(args) != 0{
+	switch strings.ToLower(commandStr) {
+	case "add":
+		if len(args) == 0 {
+			fmt.Printf("Specify argument to add a new task.\n")
+			return
+		}
 		user.AddTask(strings.Join(args, " "))
-	} else if action == "remove" && len(args) != 0{
-		// user.RemoveTask(int)
-		fmt.Printf("delete %v\n", args[0])
-	}else{
-		fmt.Printf("Action not recognized\n")
+	case "remove":
+		if len(args) == 0 {
+			fmt.Printf("Give index of task to remove\n")
+			return
+		}
+		index, err := strconv.Atoi(args[0])
+		if err != nil {
+			fmt.Printf("Error converting argument to string : %v", err)
+			return
+		}
+		user.RemoveTask(index)
+	default:
+		fmt.Printf("Unknown command\n")
 	}
 }
 
-func checkFileExistence()  error {
+func checkFileExistence() error {
 	file, err := os.OpenFile(dataDirectory, os.O_CREATE, 0644)
 	if err != nil {
-		if os.IsExist(err){
+		if os.IsExist(err) {
 			return nil
 		}
 		return err
-	} 
+	}
 	defer file.Close()
 	return nil
 }
 
-func loadDataFromFile() []byte{
+func loadDataFromFile() []byte {
 	data, err := os.ReadFile(dataDirectory)
 	if err != nil {
 		fmt.Printf("Error reading file : %v\n", err)
@@ -56,11 +68,11 @@ func loadDataFromFile() []byte{
 	return data
 }
 
-func parseUserInput(input []string) (string, []string){
+func parseUserInput(input []string) (string, []string) {
 	// case where if we call the executable with no command, error
-	if len(input) ==1  {
+	if len(input) == 1 {
 		return "", nil
 
-	}	
-	return input[1], input[2:] 
+	}
+	return input[1], input[2:]
 }
